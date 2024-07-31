7cabcc890fcabf0e5f81e226a51291e775b88806
antmus1 <68811923+antmus1@users.noreply.github.com>
2024-07-31 16:29:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ function addReminder() {
     const date = document.getElementById('date').value;
     const time = document.getElementById('time').value;
     const location = document.getElementById('location').value;
+    const text = document.getElementById('text').value;
 
-    if (date) {
-        const reminder = { date, time, location };
+    if (date && text) {
+        const reminder = { date, time, location, text };
         saveReminder(reminder);
         displayReminders();
     } else {
-        alert('Per favore, inserisci una data.');
+        alert('Per favore, inserisci una data e un testo per il promemoria.');
     }
 }
 
@@ -32,17 +33,15 @@ function displayReminders() {
     reminderItems.innerHTML = '';
 
     const today = new Date();
-    const nextWeek = new Date();
-    nextWeek.setDate(today.getDate() + 7);
 
     reminders
         .filter(reminder => {
             const reminderDate = new Date(reminder.date);
-            return reminderDate >= today && reminderDate <= nextWeek;
+            return reminderDate >= today;
         })
         .forEach(reminder => {
             const listItem = document.createElement('li');
-            listItem.textContent = `${reminder.date} ${reminder.time ? reminder.time : ''} - ${reminder.location}`;
+            listItem.textContent = `${reminder.date} ${reminder.time ? reminder.time : ''} - ${reminder.location ? reminder.location : ''} - ${reminder.text}`;
             reminderItems.appendChild(listItem);
         });
 }
